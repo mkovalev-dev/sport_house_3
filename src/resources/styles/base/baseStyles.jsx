f2ffc263e03329368805a3a38db48d13141f2fa8
maximkovalev <mkovalevhse@yandex.ru>
2022-08-01 23:47:20 +0400

    base settings

---
+++ src/resources/styles/base/baseStyles.jsx
@@ -0,0 +1,17 @@
+export const DEFAULT_COLORS = {
+    WHITE: "#fff",
+    BLACK: "#000000",
+    PRIMARY: "#f48600",
+    DARK_GRAY: "#27272a",
+    RED:"#dc2626",
+    GREEN:"#16a34a"
+}
+
+export const DEFAULT_STYLE_PARAMS = {
+    padding: 20,
+    paddingLeft: 35,
+    paddingRight: 35,
+    paddingBottom: 35,
+    paddingTop: 35,
+    borderRadius: 35,
+}
